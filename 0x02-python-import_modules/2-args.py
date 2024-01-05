c94783698678b04f91b8f83d7f051b77c6443554
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-01-05 11:31:42 -0800

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
+    """Print number of and list of args."""
     import sys
 
     count = len(sys.argv) - 1
