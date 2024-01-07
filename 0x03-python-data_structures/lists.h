ae198477813305b19612ddff622cd43b48cd5247
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-01-06 17:21:57 -0800

    is palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -12,8 +13,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,5 +22,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+
+/* helper functions for the task */
+int palindrome_helper(listint_t *list_1, listint_t *list_2);
+void reverse(listint_t **list);
 
 #endif /* LISTS_H */
