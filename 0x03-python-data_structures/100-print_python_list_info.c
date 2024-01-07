417b5024f29c21fc020dc85006f382934860496e
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-01-06 16:20:35 -0800

    Cpyton

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <Python.h>
-
 /**
- * print_python_list_info -  function that prints some basic
+ * print_python_list_info -  function prints some basic
  *							info about Pylists
- * @p: python
+ * @p: python list
  */
 void print_python_list_info(PyObject *p)
 {
