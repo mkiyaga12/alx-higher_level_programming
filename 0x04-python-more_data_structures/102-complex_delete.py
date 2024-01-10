e9113eb42b3f4db944ffc9678367b06b0e91e78d
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-01-10 14:45:15 -0800

    deletes keys with a specific value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,4 @@ def complex_delete(a_dictionary, value):
     for k, v in tmp.items():
         if value == v:
             a_dictionary.pop(k)
-            return a_dictionary
+    return a_dictionary
